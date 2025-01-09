8f1f3e41c7456783291da6f7eeafb7e49e372ebb
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2025-01-09 10:59:27 -0700

    images edited

--- app/pages/instagram.jsx
+++ app/pages/instagram.jsx
@@ -3,7 +3,7 @@ const Instagram = () => {
  
   return (
    <div id="instagram"
-   className='relative min-h-[60vh] w-full bg-[url("./images/Segment_02.jpg")] bg-cover bg-center'>
+   className='relative min-h-[60vh] w-full bg-[url("/images/Segment_02.jpg")] bg-cover bg-center' >
      <div className="absolute inset-0 bg-gradient-radial from-transparent via-black/70 to-black" />
      <div className="relative min-h-[60vh] flex flex-col justify-center items-center">
      <img className=""
