8792747de94c1f970f9acdf402d4eef25a8692d1
microbizmedia <info@microbizmedia.com>
2025-03-20 15:32:48 -0600

    Favicon.ico moved to app folder

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./styles/globals.css";
 import './styles/menu.css';
-import Head from 'next/head';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -16,18 +16,11 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Hawaii Barbershop",
   description: "Where Style Meets Aloha",
-  // icons: {
-  //   icon: '/favicon.ico',  // Try using PNG
-  // },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </Head>
-
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
