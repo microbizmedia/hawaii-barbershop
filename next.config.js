276554c5fc609e82bf6a8f700a611698dd0bb5f8
microbizmedia <info@microbizmedia.com>
2025-03-20 11:07:26 -0600

    BasePath added in nex.config.js.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-}
- 
-module.exports = nextConfig
+  output: "export", // Ensures static export for GitHub Pages
+  basePath: "/hawaii-barbershop",
+  assetPrefix: "/hawaii-barbershop/",
+};
+
+module.exports = nextConfig;
