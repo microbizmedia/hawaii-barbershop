7cb4a5dc9c8e06b49d818b08217ca161d486e308
microbizmedia <info@microbizmedia.com>
2025-03-20 10:30:30 -0600

    Deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./public/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "480px", // Custom breakpoint
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+      colors: {
+        priceColor: "#91765a",
+        backgroundColor: "#f8f3ed",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(circle, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(circle, var(--tw-gradient-stops))",
       },
     },
   },
